9f9322c90f97ba053b7f173c82867f128cab2bbd
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-05-03 18:35:29 +0000

    Done with work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function findMovieType() {
+
+  var age = parseInt(document.getElementById('ageInput').value);
+
+
+  var ageRating;
+  if (age >= 18) {
+    ageRating = "You can watch R-rated movies";
+  } else if (age >= 13) {
+    ageRating = "You can watch PG-13 rated movies";
+  } else if (age >= 6) {
+    ageRating = "You can watch PG rated movies";
+  } else {
+    ageRating = "You can only watch G-rated movies";
+  }
+
+
+  document.getElementById("ageRating").innerHTML = "<p>" + ageRating + "</p>";
+}
